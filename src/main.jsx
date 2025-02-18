072e3732893d3bc57c704d9491142b7bf0569906
Aleksandr-1977 <vasilenkobox@gmail.com>
2025-02-18 16:56:07 +0200

    practice

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import reactLogo from './assets/react.svg';
+import viteLogo from '../public/vite.svg';
+import './index.css';
+import App from './components/App';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);

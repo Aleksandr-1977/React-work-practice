7260b29c97aead47f6148c79f4afd01af6470988
Aleksandr-1977 <vasilenkobox@gmail.com>
2025-03-06 16:42:10 +0200

    add formik

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,30 +3,79 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import LoginForm from './Product';
 import * as Yup from 'yup';
-import { Formik } from 'formik';
+
+import { useId } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
+const FeedbackSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  email: Yup.string().email('Must be a valid email!').required('Required'),
+  message: Yup.string()
+    .min(3, 'Too short')
+    .max(256, 'Too long')
+    .required('Required'),
+  level: Yup.string().oneOf(['good', 'neutral', 'bad']).required('Required'),
+});
+
+const initialValues = {
+  username: '',
+  email: '',
+  message: '',
+  level: 'good',
+};
 
 const App = () => {
-  const [hasAccepted, setHasAccepted] = useState(false);
+  const nameFieldId = useId();
+  const emailFieldId = useId();
+  const msgFieldId = useId();
+  const levelFieldId = useId();
 
-  const handleChange = evt => {
-    setHasAccepted(evt.target.checked);
+  const handleSubmit = (values, actions) => {
+    console.log(values);
+    actions.resetForm();
   };
 
   return (
-    <div>
-      <label>
-        <input
-          type="checkbox"
-          name="terms"
-          checked={hasAccepted}
-          onChange={handleChange}
-        />
-        I accept terms and conditions
-      </label>
-      <button type="button" disabled={!hasAccepted}>
-        Proceed
-      </button>
-    </div>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={FeedbackSchema}
+    >
+      <Form>
+        <div>
+          <label htmlFor={nameFieldId}>Username</label>
+          <Field type="text" name="username" id={nameFieldId} />
+          <ErrorMessage name="username" component="span" />
+        </div>
+
+        <div>
+          <label htmlFor={emailFieldId}>Email</label>
+          <Field type="email" name="email" id={emailFieldId} />
+          <ErrorMessage name="email" component="span" />
+        </div>
+
+        <div>
+          <label htmlFor={msgFieldId}>Message</label>
+          <Field as="textarea" name="message" id={msgFieldId} rows="5" />
+          <ErrorMessage name="message" component="span" />
+        </div>
+
+        <div>
+          <label htmlFor={levelFieldId}>Service satisfaction level</label>
+          <Field as="select" name="level" id={levelFieldId}>
+            <option value="good">Good</option>
+            <option value="neutral">Neutral</option>
+            <option value="bad">Bad</option>
+          </Field>
+          <ErrorMessage name="level" component="span" />
+        </div>
+
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
   );
 };
 // const App = () => {
